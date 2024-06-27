d3fba321a355c3a29f6422feba30b308ce29df02
Gokul-Da <gokul.tn.ind@gmail.com>
2024-06-27 09:29:43 +0530

    Initial Commit
    Simple Calculator

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.simplecalculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView display;
+    private String currentOperator;
+    private double firstNumber, secondNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+
+        setNumberButtonListeners();
+        setOperatorButtonListeners();
+    }
+
+    private void setNumberButtonListeners() {
+        int[] numberButtonIds = {
+                R.id.button0, R.id.button1, R.id.button2, R.id.button3,
+                R.id.button4, R.id.button5, R.id.button6, R.id.button7,
+                R.id.button8, R.id.button9
+        };
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                display.append(button.getText().toString());
+            }
+        };
+
+        for (int id : numberButtonIds) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void setOperatorButtonListeners() {
+        int[] operatorButtonIds = {
+                R.id.buttonAdd, R.id.buttonSubtract, R.id.buttonMultiply, R.id.buttonDivide
+        };
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                firstNumber = Double.parseDouble(display.getText().toString());
+                currentOperator = button.getText().toString();
+                display.setText("");
+            }
+        };
+
+        for (int id : operatorButtonIds) {
+            findViewById(id).setOnClickListener(listener);
+        }
+
+        findViewById(R.id.buttonEquals).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                secondNumber = Double.parseDouble(display.getText().toString());
+                double result = 0;
+
+                switch (currentOperator) {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber != 0) {
+                            result = firstNumber / secondNumber;
+                        } else {
+                            display.setText("Error");
+                            return;
+                        }
+                        break;
+                }
+                display.setText(String.valueOf(result));
+            }
+        });
+
+        findViewById(R.id.buttonClear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText("");
+                firstNumber = 0;
+                secondNumber = 0;
+                currentOperator = "";
+            }
+        });
+    }
+}
